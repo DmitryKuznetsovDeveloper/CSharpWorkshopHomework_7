c787f423ff904ec8c5b0658d06d84c336296a838
DmitriyKuznecov-azur <kuznecov.d@azurgames.com>
2023-03-04 22:00:41 +0300

    added solution Task003

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int InputUser(string message)
+{
+    Console.Write($"{message} => ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Метод создания двумерного массива
+int[,] CreationMatrix(int row, int col)
+{
+    int[,] array = new int[row, col];
+    return array;
+}
+
+// Метод заполнения массива псевдорандомными числами
+int[,] FillMatrix(int[,] array)
+{
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 21);
+        }
+    }
+    return array;
+}
+
+// Метод вывода массива
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+    }
+    Console.WriteLine();
+}
+
+// Метод выводит среднеарифметическое значение по колонкам в двумерном массиве
+void AverageColumMatrix(int[,] array)
+{
+    Console.WriteLine();
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double average = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            average += array[i, j];
+        }
+        Console.WriteLine($"В колонке {j + 1} = {Math.Round(average / array.GetLength(0), 1)} ");
+    }
+}
+
+// Обработка исключений
+try
+{
+    int userRow = InputUser("Введите количество строк в двумерном массиве");
+    int userCol = InputUser("Введите количество колонок в двумерном массиве");
+    int[,] userMatrix = CreationMatrix(userRow, userCol);
+    PrintMatrix(FillMatrix(userMatrix));
+    Console.WriteLine($"Cреднеарифметическое значения в колонках :");
+    AverageColumMatrix(userMatrix);
+}
+catch (FormatException)
+{
+    Console.WriteLine("Вы ввели не число =(");
+}
+
